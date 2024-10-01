3886c7f8b754ef8fe5dfc07a8241182a3e9794fb
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2024-10-01 11:58:29 +0200

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataform-rate",
+    version="0.1.0",
+    description="Simple tool to test your Dataform project",
+    author="Mchl Schrdng",
+    author_email="mchl.schrdng@gmail.com",
+    url="https://github.com/mchl-schrdng/dataform-rate",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",  
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
